cdacf4930679759ba02a85979a933c72625b2fac
SamDreamsMaker <samdreamsmaker@gmail.com>
2023-02-23 11:28:28 -0500

    stop to use aspose

--- Assets/Scripts/FlitPicture.cs
+++ Assets/Scripts/FlitPicture.cs
@@ -1,16 +1,30 @@
 using Aspose.Imaging;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FlitPicture : MonoBehaviour
 {
     [SerializeField]
     GameObject message;
-    // Start is called before the first frame update
+    static string subFolderToFlip = "toFlip";
+    static string subFolderFlipped= "flipped";
+    [SerializeField]
+    Toggle rescale1820Toggle;
+    [SerializeField]
+    Button flipButton;
     public void FlipPictures()
     {
+        /*
+        string licenseFile = Path.Combine(Application.streamingAssetsPath, "Aspose.Total.lic");
+        License license = new License();
+        license.SetLicense(licenseFile);
+        */
+
+        flipButton.interactable = false;
         DisableMessage();
         List<string> extensions = new List<string>();
         extensions.Add("*.png");
@@ -23,31 +37,64 @@ public class FlitPicture : MonoBehaviour
         extensions.Add("*.gif");
 
         if (Directory.Exists(Application.streamingAssetsPath)) {
-            string worldsFolder = Application.streamingAssetsPath;
+            string worldsFolder = Path.Combine(Application.streamingAssetsPath, subFolderToFlip);
             Debug.Log(worldsFolder);
             DirectoryInfo d = new DirectoryInfo(worldsFolder);
             foreach (string e in extensions) {
                 foreach (FileInfo file in d.GetFiles(e, SearchOption.AllDirectories)) {
-                    Flip(file.FullName, file.Name, file.Extension, "_flip");
+                    string destination = file.FullName.Replace(subFolderToFlip, subFolderFlipped).Replace(file.Name,"");
+                    ReversePixels(file.FullName, destination, file.Name);
                 }
             }
         }
         message.SetActive(true);
+        flipButton.interactable = true;
         Invoke("DisableMessage", 5.0f);
     }
     void DisableMessage() {
         message.SetActive(false);
     }
-    // Update is called once per frame
-    void Flip(string fullpath, string nameFile,string extension,string suffix)
+    void Flip(string fileLocation, string destination, string nameFile,string extension)
     {
-        Debug.Log(fullpath);
-        using (Image image = Image.Load(fullpath)) {
-            // Flip the image
+        using (Aspose.Imaging.Image image = Aspose.Imaging.Image.Load(fileLocation)) {
             image.RotateFlip(RotateFlipType.RotateNoneFlipX);
-            // Save image
-            image.Save(fullpath + suffix + extension);
+            if (rescale1820Toggle.isOn) {
+                image.ResizeWidthProportionally(1820);
+            }
+            image.Save(destination);
         }
+    }
+
+    void ReversePixels(string fileLocation, string destination, string fileName) {
+        byte[] fileData = File.ReadAllBytes(fileLocation);
+
+        // Créer la texture à partir des données brutes
+        Texture2D texture = new Texture2D(2, 2);
+        texture.LoadImage(fileData);
+
+        // Obtenir les pixels de la texture
+        UnityEngine.Color[] pixels = texture.GetPixels();
+
+        // Inverser l'ordre des pixels horizontalement
+        for (int y = 0; y < texture.height; y++) {
+            Array.Reverse(pixels, y * texture.width, texture.width);
+        }
+
+        // Recréer la texture avec les pixels inversés
+        Texture2D flippedTexture = new Texture2D(texture.width, texture.height);
+        flippedTexture.SetPixels(pixels);
+        flippedTexture.Apply();
+
+        // Sauvegarder la texture inversée en JPG
+        byte[] jpgData = flippedTexture.EncodeToJPG();
+        string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
+
+        if (!Directory.Exists(destination)) {
+            // Créer le dossier s'il n'existe pas
+            Directory.CreateDirectory(destination);
+        }
+        string filePath = Path.Combine(destination, fileNameWithoutExtension+".jpeg");
+        File.WriteAllBytes(filePath, jpgData);
     }
 
 }
